4ef5c5d0fec44977e51d65fa91da707728b0781e
RohitMachiwal <rohitmachiwal@yahoo.com>
2022-06-23 11:42:31 +0530

    Hystric-Dashboard

---
+++ Hystrix_Dashboard/src/test/java/com/hdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
